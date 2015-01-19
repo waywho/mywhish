28a413852cf6af54141bea20a455020acd70cc31
WeiHsi Hu <weihsi.hu@gmail.com>
2015-01-19 21:13:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mywish::Application.config.secret_key_base = '9ed43e873671753e83dd7dd01cc56959542b1db992d8a0b65240be826b3c236df2eaa5af1e4a593b84d20d54e492235795c85b2f737350e17d9f3f3d6946d1a6'
